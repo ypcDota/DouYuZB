238c8cdf6492f84d1fd2ea72956762d7af046e17
PCweibo <985348196@qq.com>
2017-01-02 10:07:00 +0800

    tabBar的设置

---
+++ 斗鱼直播/斗鱼直播/Classes/Main/MainViewController.swift
@@ -0,0 +1,33 @@
+//
+//  MainViewController.swift
+//  斗鱼直播
+//
+//  Created by ypc on 17/1/2.
+//  Copyright © 2017年 com.ypc. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // 添加自控制器
+        // 通过 storyBoard 加载
+        addChildVc(VcName: "Home")
+        addChildVc(VcName: "Live")
+        addChildVc(VcName: "Follow")
+        addChildVc(VcName: "Profile")
+
+    }
+    
+    // 添加自控制器 抽取函数
+    private func addChildVc(VcName : String) {
+        
+        let childVc = UIStoryboard(name: VcName, bundle: nil).instantiateInitialViewController()!
+        addChildViewController(childVc)
+    
+    }
+
+}
